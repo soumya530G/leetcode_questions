0f2d653a445d03b89692301c9f75129086a26eea
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-03-30 18:28:24 +0530

    Time: 42 ms (28.97%), Space: 45.5 MB (36.46%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,7 +2,7 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int []dp = new int[amount + 1];
         coinchange(coins, amount, dp);
-        if(dp[amount] == 1e9){
+        if(dp[amount] == 1e5){
             return -1;
         }
         return dp[amount];
@@ -14,7 +14,7 @@ class Solution {
         if(dp[amount] != 0){
             return dp[amount];
         }
-        dp[amount] = (int)1e9;
+        dp[amount] = (int)1e5;
         for(int i: coins){
             if(amount >= i){
                dp[amount] = Math.min(dp[amount], coinchange(coins, amount - i, dp) + 1);

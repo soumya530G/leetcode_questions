edf9add40abac5c834533276c91d518c5c627cd4
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-04-06 21:45:11 +0530

    Time: 2 ms (80.51%), Space: 48 MB (65.60%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -4,16 +4,8 @@ class Solution {
         for(int i = 0; i < n; i++){
             ans[i] = 0;
         }
-        for(int i = 1; i < n + 1; i++){
-            //even 
-            if(i % 2 == 0){
-               ans[i] = ans[i / 2]; 
-            }
-            //x/2
-            else{
-                ans[i] = 1 + ans[i / 2];
-            }
-            
+        for(int i = 1; i < n + 1; i++){ 
+               ans[i] = ans[i / 2] + i % 2;   
         }
         return ans;
     }

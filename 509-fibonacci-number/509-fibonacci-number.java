b2f22b1dc60f789c6a14f16b15ab20397cb82f23
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-05-03 18:04:09 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (69.94%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,9 +1,16 @@
 class Solution {
     public int fib(int n) {
+      return fibmem(n, new int[n + 1]);
+    }
+    public int fibmem(int n, int[] dp){
         if(n == 0 || n == 1){
             return n;
         }
-        int fibn = fib(n-1) + fib(n-2);
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        int fibn = fibmem(n - 1, dp) + fibmem(n - 2, dp);
+        dp[n] = fibn;
         return fibn;
     }
 }

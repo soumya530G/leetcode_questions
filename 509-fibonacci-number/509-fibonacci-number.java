426dcf16f547e8eeb3c2b184240d614c4c325c48
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-05-03 18:01:51 +0530

    Time: 18 ms (6.85%), Space: 41.1 MB (29.43%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,17 +1,9 @@
 class Solution {
     public int fib(int n) {
-    if(n == 0){
-        return 0;
-    }
-    int a = 0;
-    int b = 1;
-    int c = a + b;
-    for(int i = 0; i < n - 1; i++){
-         c = a + b;
-         a = b;
-         b = c;  
-         System.out.println(c);
-       }
-    return c; 
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int fibn = fib(n-1) + fib(n-2);
+        return fibn;
     }
 }

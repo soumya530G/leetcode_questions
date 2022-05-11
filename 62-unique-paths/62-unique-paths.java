9874d6ab1e04f63a547d8a62cbd488895a7767ed
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-05-11 18:38:35 +0530

    Time: 1 ms (41.47%), Space: 40.6 MB (63.41%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -4,7 +4,7 @@ class Solution {
       for(int i = 0; i < n; i++){
           for(int j = 0; j < m; j++){
               if(i == 0){
-                  dp[0][j] = 1;
+                  dp[i][j] = 1;
               }
               else if(j == 0){
                   dp[i][j] = 1;

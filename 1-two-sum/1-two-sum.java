55aa075551e180a4184bf5a6b96b91044e7d4623
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-06-01 12:33:32 +0530

    Time: 23 ms (43.65%), Space: 47.4 MB (5.46%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n1 = 0;
+        int n2 = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int fq = map.get(nums[i]);
+                int nf = fq + 1;
+                map.put(nums[i], nf);
+            }else{
+              map.put(nums[i], 1);
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int f = map.get(nums[i]);
+                int nff = f - 1;
+                map.put(nums[i], nff);
+            }
+            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) > 0){
+                n1 = nums[i];
+                n2 = target - nums[i];
+                break;
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == n1 || nums[i] == n2){
+                ans.add(i);
+            }
+        }
+        
+         for (int i = 0; i < ans.size(); i++){
+             System.out.print(ans.get(i) + " ");
+         }
+        System.out.println();
+         int[] arr = new int[ans.size()];
+          for (int i = 0; i < ans.size(); i++)
+            arr[i] = ans.get(i);
+        return arr;
+    }
+}

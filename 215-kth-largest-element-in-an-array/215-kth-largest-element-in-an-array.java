d2b13a3c9a2d75b1a0c1703a1958c7b76aebd28f
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-06-02 00:20:22 +0530

    Time: 5 ms (64.88%), Space: 45.6 MB (10.53%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -4,26 +4,18 @@ import java.util.Collections;
 import java.util.Scanner;
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-       // HashMap<Integer, Integer> map = new HashMap<>();
-          Arrays.sort(nums);
-      /*  int count = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i]) == false){
-                map.put(nums[i], count);
-                count++;
+            if(pq.size() < k)
+            pq.add(nums[i]);
+            else{
+                if(pq.peek() < nums[i]){
+                   pq.remove();
+                    pq.add(nums[i]);
+                }
             }
-        }*/
-        int n = nums.length - k;
-        return nums[n];
-      /*  int q = Math.abs(count - 1 - k);
-        System.out.println(count + ":" + q);
-        int ans = 0;
-        for(Entry<Integer, Integer> entry: map.entrySet()){
-            if(entry.getValue() == q){
-                ans = entry.getKey();
-                break;
-            }
-        }*/
-      //  return ans;
+        }
+         return pq.peek();
+        
     }
 }

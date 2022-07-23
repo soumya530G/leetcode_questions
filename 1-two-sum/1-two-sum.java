6aed8c12c17fe34cd650f6038a1e9374c5e818d0
Soumya Gupta <78527655+soumya530G@users.noreply.github.com>
2022-07-23 22:50:32 +0530

    Time: 14 ms (45.03%), Space: 48.4 MB (5.95%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -30,11 +30,6 @@ class Solution {
                 ans.add(i);
             }
         }
-        
-         for (int i = 0; i < ans.size(); i++){
-             System.out.print(ans.get(i) + " ");
-         }
-        System.out.println();
          int[] arr = new int[ans.size()];
           for (int i = 0; i < ans.size(); i++)
             arr[i] = ans.get(i);
